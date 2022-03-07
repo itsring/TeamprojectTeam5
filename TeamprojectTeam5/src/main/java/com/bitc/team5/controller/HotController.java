4c871b3143347def6bb88f08babf32f2511991cd
1yena <elmocize1@naver.com>
2022-03-07 21:06:31 +0900

    22-03-07

---
+++ TeamprojectTeam5/src/main/java/com/bitc/team5/controller/HotController.java
@@ -0,0 +1,39 @@
+package com.bitc.team5.controller;
+
+//@Controller
+//public class HotController {
+//	
+//	@Autowired
+//	private HotService hotService;
+//	
+/////////////////****************관광지 리스트 페이지*****************///////////////////
+//	@RequestMapping(value="/hotplace")
+//	public ModelAndView hotplace() throws Exception {
+//		ModelAndView mv = new ModelAndView("/hot/hotmain");
+//		
+//		List<HotDto> hotList = hotService.selectHotList();
+//		mv.addObject("hotList", hotList);
+//		
+//		return mv;
+//	}
+//	
+//	
+/////////////////****************관광지 등록 페이지*****************///////////////////
+//	//상품등록 페이지
+//		@RequestMapping(value="/hotinsert")
+//		public String hotinsert() throws Exception {
+//		
+//		return "/hot/hotinsert";
+//		}
+//		
+//		//상품 정보 입력
+//		@RequestMapping(value="/hotinsert/hotRegister")
+//		public String proregister(HotDto hotPlace) throws Exception {
+//			hotService.hotinsert(hotPlace);
+//		
+//			return "redirect:/hotplace";
+//		}
+//	
+//	
+//
+//}

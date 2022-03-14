61f298f7e205605eeaa7277e711873fca4307c4f
KDY <merry4739@naver.com>
2022-03-14 12:19:15 +0900

    lodge3

--- TeamprojectTeam5/src/main/java/com/bitc/team5/controller/LodgeController.java
+++ TeamprojectTeam5/src/main/java/com/bitc/team5/controller/LodgeController.java
@@ -3,15 +3,23 @@ package com.bitc.team5.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bitc.team5.dto.LodgeDto;
 import com.bitc.team5.dto.LodgeRoomDto;
+import com.bitc.team5.dto.lodgeBookDto;
 import com.bitc.team5.service.lodge.LodgeService;
 
 @Controller
@@ -33,11 +41,12 @@ public class LodgeController {
 	}
 
 	
-	////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////
+	///////
 	
 	
 	/* 숙소 찾기 페이지(지도)2*/
-	@RequestMapping(value="/lodge/lodgeSearch2{}", method=RequestMethod.GET) 
+	@RequestMapping(value="/lodge/lodgeSearch2", method=RequestMethod.GET) 
 	public String lodgeSearch2() throws Exception { 
 		return "/lodge/lodgeSearch2"; 
 	}
@@ -69,7 +78,7 @@ public class LodgeController {
 		String lodegName = (String) param.get("lodgeName");	
 		System.out.println(param.get("lodgeName"));
 		
-		return "/lodge/lodgeBook2"; 
+		return "lodgeName"; 
 	}
 
 	
@@ -88,18 +97,54 @@ public class LodgeController {
 //	}
 	
 	/* 숙소 확인 페이지3 + 객실 조회*/
-	@RequestMapping(value="/lodge/lodgeBook3", method=RequestMethod.GET) 
-	public ModelAndView lodgeRoomList() throws Exception {
-		ModelAndView mv = new ModelAndView("/lodge/lodgeBook3");
+//	@RequestMapping(value="/lodge/lodgeBook3", method=RequestMethod.GET) 
+//	public ModelAndView lodgeRoomList() throws Exception {
+//		ModelAndView mv = new ModelAndView("/lodge/lodgeBook3");
+//		
+//		List<LodgeRoomDto> roomList = lodgeService.selectRoomList();
+//		mv.addObject("roomList", roomList);
+//		
+//		return mv;
+//
+//	}
+	
+	@RequestMapping(value="/lodge/lodgeBook3", method=RequestMethod.GET)
+	public ModelAndView lodgeRoomList(HttpServletRequest request, Model model) throws Exception {
+		String aName= request.getParameter("aName");
+		String pName= request.getParameter("pName");
+		String phone= request.getParameter("phone");
+		HttpSession session = request.getSession();
+		session.setAttribute("aName", aName);
+		session.setAttribute("pName", pName);
+		session.setAttribute("phone", phone);
+//		print print = new print();
 		
+//		System.out.println(aName+", "+pName+", "+phone);
+//		System.out.println(session.getAttribute("email")+", "+session.getAttribute("aName")+", "+session.getAttribute("pName")+", "+session.getAttribute("phone"));
+		
+		
+		ModelAndView mv = new ModelAndView("/lodge/lodgeBook3");
 		List<LodgeRoomDto> roomList = lodgeService.selectRoomList();
+		
+//		숙소 이름에 맞는 이미지 출력
+//		System.out.println(pName);
+		String imgUrl = null;
+		for (LodgeRoomDto room : roomList) {
+			if(room.getLodgeName().equals(pName)) {
+				imgUrl = room.getLodgeImg();
+			}
+		}
+		
+//		System.out.println(imgUrl);
 		mv.addObject("roomList", roomList);
+		mv.addObject("imgUrl", imgUrl);
 		
 		return mv;
-
+		
 	}
 	
-//	@RequestMapping(value="/lodge/lodgeBook2", method=RequestMethod.GET)
+
+//	@RequestMapping(value="/lodge/lodgeBook3", method=RequestMethod.GET)
 //	public String lodgeBook2(HttpServletRequest request, Model model) throws Exception {
 //		String aName= request.getParameter("aName");
 //		String pName= request.getParameter("pName");
@@ -110,12 +155,13 @@ public class LodgeController {
 //		session.setAttribute("phone", phone);
 //		print print = new print();
 //		
-//		System.err.println(aName+", "+pName+", "+phone);
+//		System.out.println(aName+", "+pName+", "+phone);
 //		System.out.println(session.getAttribute("email")+", "+session.getAttribute("aName")+", "+session.getAttribute("pName")+", "+session.getAttribute("phone"));
 //		
-//		return "/lodge/lodgeBok2";
+//		return "/lodge/lodgeBook3";
 //		
 //	}
+	
 	
 //	@RequestMapping(value="/lodge/lodgeBook3?pName={name}&aName={addrname}&phone={phone}", method=RequestMethod.GET) 
 //	public ModelAndView lodgeRoomList() throws Exception {
---
+++ TeamprojectTeam5/src/main/java/com/bitc/team5/dto/lodgeBookDto.java
@@ -0,0 +1,12 @@
+package com.bitc.team5.dto;
+
+import lombok.Data;
+
+
+@Data
+public class lodgeBookDto {
+	private String name;
+	private String addrname;
+	private int phone;
+
+}
